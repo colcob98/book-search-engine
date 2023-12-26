11dbc485b86078535b824725b8183434053c2d90
Collier Cobb <collierj.cobb@gmail.com>
2023-12-26 14:11:35 -0500

    refactored auth.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
