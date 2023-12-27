42418f2236cf3c7cd4d1dc65aa0c163fc904103d
Collier Cobb <collierj.cobb@gmail.com>
2023-12-26 20:02:26 -0500

    fixing resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,19 @@
-const userController = require('../controllers/user-controller');
+const userController = require("../controllers/user-controller");
+const { User, Book } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: userController.getSingleUser,
   },
   Mutation: {
-    login: userController.login,
-    addUser: userController.createUser,
+    login: async () => {
+      userController.login;
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const body = { username, email, password };
+      return userController.createUser({ body });
+    },
     saveBook: userController.saveBook,
     removeBook: userController.deleteBook,
   },
